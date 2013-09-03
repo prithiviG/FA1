b8277b995ee2dcc26933586053802ddbb8cd6171
prithiviG <prithivigovindan@yahoo.com>
2013-09-03 14:39:22 +0530

    common

---
+++ FA/App_Code/BL/CityBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using FA.DataLayer;
+
+namespace FA.BusinessLayer
+{
+    class CityBL
+    {
+
+        internal static int CityId { get; set; }
+
+        internal static int StateId { get; set; }
+
+
+        internal static int CountryId { get; set; }
+
+        internal static String CityName { get; set; }
+
+
+        internal static DataTable GetCityList()
+        {
+            return CityDL.GetCityList();
+        }
+
+        internal static void UpdateCity()
+        {
+            CityDL.UpdateCity();
+        }
+
+        internal static void InsertCity()
+        {
+            CityDL.InsertCity();
+        }
+
+    }
+}
